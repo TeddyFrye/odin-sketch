1ef9e8d239282cc07fcf79ec13f799d8d31406d7
TeddyFrye <tfrye1227@gmail.com>
2023-04-28 09:25:47 -0500

    Adds darkening hover effect

--- script.js
+++ script.js
@@ -8,7 +8,22 @@ for (let i = 0; i < 16; i++){
     for (let j = 0; j < 16; j++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
+        gridSquare.dataset.darkness = 0;
         row.appendChild(gridSquare);
     }
     container.appendChild(row);
 }
+function darkenSquare(e) {
+    const darkness = parseInt(e.target.dataset.darkness) + 30;
+    e.target.style.backgroundColor = `hsl(0, 0%, ${100 - darkness}%)`;
+    e.target.dataset.darkness = darkness;
+  }
+const squares = document.querySelectorAll('.grid-square');
+squares.forEach(square => {
+    square.addEventListener('mouseover', darkenSquare);
+});
+/*
+rockBtn.addEventListener('mouseover', function() {
+    rockBtn.style.backgroundColor = 'lightgray';
+});
+*/
